d64b414d99605ddd3cd70abc192d0cec70f96921
Dan Park <danielpark.487@gmail.com>
2017-02-22 17:31:21 -0500

    changed some db stuff

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,21 @@ const Sequelize = require('sequelize');
 
 console.log('Opening connection to PostgreSQL');
 
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  false
+  });
+} else {
+  // the application is executed on the local machine
+  sequelize = new Sequelize('postgres://localhost:5432/trolldota', {
+    logging: false
+  });
+}
+
 // create the database instance
-module.exports = new Sequelize('postgres://localhost:5432/trolldota', {
-  logging: false // set to console.log to see the raw SQL queries
-});
+module.exports = sequelize;
