347a8f4d5a306565cbcd50dd22a4a9b415f8801b
Dan Park <danielpark.487@gmail.com>
2017-02-22 18:38:12 -0500

    changed http

--- browser/react/action-creators/player.js
+++ browser/react/action-creators/player.js
@@ -18,10 +18,10 @@ const settingPlayer = (player, winloss, matches, wordcloud) => {
 export const loadCurrentPlayer = (playerId) => {
     return dispatch => {
         Promise.all([
-            axios.get(`http://api.opendota.com/api/players/${playerId}`),
-            axios.get(`http://api.opendota.com/api/players/${playerId}/wl`),
-            axios.get(`http://api.opendota.com/api/players/${playerId}/matches?limit=25`),
-            axios.get(`http://api.opendota.com/api/players/${playerId}/wordcloud`)
+            axios.get(`https://api.opendota.com/api/players/${playerId}`),
+            axios.get(`https://api.opendota.com/api/players/${playerId}/wl`),
+            axios.get(`https://api.opendota.com/api/players/${playerId}/matches?limit=25`),
+            axios.get(`https://api.opendota.com/api/players/${playerId}/wordcloud`)
         ])
             .then(results => results.map(r => r.data))
             .then(results => {
